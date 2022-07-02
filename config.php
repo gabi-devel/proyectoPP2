3293b5fa29b24db881eb19be9c7b67acacc992d0
Gabi <gabrielabustos07@gmail.com>
2022-07-01 23:23:31 -0300

    Template

--- config.php
+++ config.php
@@ -1,7 +1,16 @@
 <?php
-
-$conexion = mysqli_connect("localhost", "root", "", "proyecto"); 
-
-//if($conexion) echo "Se ha conectado a la base de datos correctamente";
-
+$dsn = 'mysql:dbname=admin01;host=localhost';
+$user = 'root';
+$password = '';
+ 
+try
+{
+	$pdo = new PDO($dsn,$user,$password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+	echo "PDO error".$e->getMessage();
+	die();
+}
 ?>
